7bdcffd0dcd11f048ab228c53698d2df49265871
João Pedro <joaopedrosilvadossantos2003@gmail.com>
2021-10-12 20:17:53 -0300

    uu

--- board/src/index.js
+++ board/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import App from './pages/registrations';
+import App from './pages/home/index';
 
 ReactDOM.render(
   <React.StrictMode>
