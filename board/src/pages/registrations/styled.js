25e7674f957d37939a6ef7a88f975e0c5515f6d8
Bruninho <bruno.souza.c2014@gmail.com>
2021-10-12 20:05:05 -0300

    amem

---
+++ board/src/pages/registrations/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components"
+
+const Containerinc = styled.div` 
+display: flex;
+justify-content: space-between;
+`
+const ContainerINS = styled.div`
+width: 100vw;
+height: 200vh;
+background-color: #F0FFFE;
+.gra1 {
+    width: 77vw;
+    left: 275px;
+    position: absolute;
+    background-color: white;
+    margin: 8em 0px;
+}
+
+.gra2 {
+    width: 77vw;
+    left: 275px;
+    position: absolute;
+    margin-top: 35%;
+    background-color: white;
+    
+}
+
+
+`
+
+export {Containerinc, ContainerINS}
